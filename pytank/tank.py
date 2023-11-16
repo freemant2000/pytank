ee3cab364f7cd946c95cc3b9a7f5fa35ecc4966e
Kent Tong <kent.tong.mo@gmail.com>
2023-11-16 12:50:48 +0800

    pending dist and angle

--- pytank/tank.py
+++ pytank/tank.py
@@ -10,12 +10,16 @@ class Tank(QGraphicsPixmapItem):
     h=40
     center_h=0.4*h
     center_to_top=h-center_h
+    step=4
+    step_degree=3
     def __init__(self, gs: QGraphicsScene, x: int, y: int):
         pm=QPixmap("pytank/tank.png")
         pm2=pm.scaled(Tank.w, Tank.h, Qt.AspectRatioMode.IgnoreAspectRatio, Qt.TransformationMode.SmoothTransformation)
         super().__init__(pm2)
         self.setOffset(-Tank.w//2, -Tank.center_to_top)
         self.gs=gs
+        self.pending_dist=0
+        self.pending_angle=0
         self.heading=0
         self.setPos(x, y)
         self.gs.addItem(self)
@@ -27,31 +31,35 @@ class Tank(QGraphicsPixmapItem):
         self.setRotation(self.heading)
 
     async def on_ready(self):
-        #await self.move(200)
-        #Bullet(self.gs, self.x(), self.y())
         await self.turn(30)
-        dx, dy=get_xy_proj(Tank.center_to_top, self.heading)
-        Bullet(self.gs, self.x()+dx, self.y()+dy, self.heading)
+        await self.move(200)
+        #Bullet(self.gs, self.x(), self.y())
+        #await self.turn(30)
+        # dx, dy=get_xy_proj(Tank.center_to_top, self.heading)
+        # Bullet(self.gs, self.x()+dx, self.y()+dy, self.heading)
         pass
         
     async def update(self):
         while True:
             await sleep(0.1)
-            await self.on_update()
-    async def on_update(self):
-        pass
+            if self.pending_dist!=0:
+                sign=1 if self.pending_dist>0 else -1
+                s=min(abs(self.pending_dist), Tank.step)
+                dx, dy=get_xy_proj(s, self.heading)
+                self.setPos(self.x()+dx, self.y()+dy)
+                self.pending_dist-=sign*s
+            if self.pending_angle!=0:
+                sign=1 if self.pending_angle>0 else -1
+                s=min(abs(self.pending_angle), Tank.step_degree)
+                self.set_heading(self.heading+sign*s)
+                self.pending_angle-=sign*s
+
     async def move(self, d: int):
-        for e in range(d//4):
-            await sleep(0.1)
-            self.setX(self.x()+4)
+        self.pending_dist=d
+        while self.pending_dist!=0:
+            await sleep(0.01)
 
     async def turn(self, angle: int):
-        step_degree=3
-        if angle>0:
-            sign=1
-        else:
-            sign=-1
-            angle=-angle
-        for e in range(angle//step_degree):
-            await sleep(0.1)
-            self.set_heading(self.heading+sign*step_degree)
+        self.pending_angle=angle
+        while self.pending_angle!=0:
+            await sleep(0.01)
