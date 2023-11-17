311ceb6a7d95af43ad4e6ccb00ca8ec04bbb566a
Kent Tong <kent.tong.mo@gmail.com>
2023-11-17 16:27:34 +0800

    update single tank game

--- pytank/single_tank.py
+++ pytank/single_tank.py
@@ -1,26 +1,22 @@
 from pytank.game import start_game
 from pytank.tank import Tank
-from PySide6.QtWidgets import QGraphicsScene
 
 the_tank: Tank = None
 
-class TheTank(Tank):
-    def __init__(self, gs: QGraphicsScene, x: int, y: int):
-        import __main__
-        self.mod=vars(__main__)
-        super().__init__(gs, x, y)
-    async def on_ready(self):
-        await self.call_async_handler("on_ready")
-    async def call_async_handler(self, fn):
-        handler=self.mod.get(fn)
+async def on_tank_ready(tank: Tank):
+    import __main__
+    mod=vars(__main__)
+    async def call_async_handler(fn):
+        handler=mod.get(fn)
         if handler:
             await handler()
+    await call_async_handler("on_ready")
 
 def start():
-    def on_ready(game):
+    def on_game_ready(game):
         global the_tank
-        the_tank=TheTank(game.gs, 200, 300)
-    start_game(on_ready)
+        the_tank=Tank(game.gs, 200, 300, on_tank_ready)
+    start_game(on_game_ready)
 
 async def move(d: int):
     await the_tank.move(d)
